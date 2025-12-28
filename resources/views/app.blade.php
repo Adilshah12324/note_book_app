a20baf3103f9c069c9a02ccf108f37336cb07234
adilkhan12324 <adilkhan12324@gmail.com>
2025-12-28 19:31:29 +0500

    my first commit after setup the laravel+inertia+react

---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Note Book App</title>
+    @viteReactRefresh 
+
+    @vite('resources/js/app.jsx')
+    @inertiaHead
+</head>
+<body>
+    @inertia
+</body>
+</html>
